db29a6e8ee16ac466a659ba820cf1a7a46de50c0
Christian <chrnielsen2003@gmail.com>
2022-02-28 17:58:43 +0100

    .

--- src/pages/VotingSite.js
+++ src/pages/VotingSite.js
@@ -10,29 +10,14 @@ import { UserContext } from '../UserContext';
 export default function VotingSite() {
     const { user } = useContext(UserContext)
     const [medarbejdere, setMedarbejdere] = useState([])
-    const [voterble, setVoterble] = useState([]);
     const [alreadyVoted, setAlreadyVoted] = useState(false);
-    const [alfa, setAlfa] = useState(false)
 
     if(user === null) {
         navigate("/")
         // eslint-disable-next-line
         location.reload();
     }
-useEffect(() => {
 
-    if(user.id === 286828){
-        setAlfa(true)
-    } else {
-        setAlfa(false)
-    }
-}, [user])
-
-
-    const fullName = user?.name;
-    const splitName = fullName.split(',')
-    const firstPartOfName = splitName[1];
-    const firstName = firstPartOfName.split(" ")[1]
 
     
     useEffect(()=> {
@@ -41,13 +26,7 @@ useEffect(() => {
         .then(response => setMedarbejdere(response.data))
     }, [])
     
-    useEffect(()=> {
-        
-        setVoterble(medarbejdere.filter((e)=> e.leader === false))
-        
-    }, [medarbejdere, setMedarbejdere]); 
-    
-    /* console.log(medarbejder); */
+    console.log(medarbejdere);
     
     
     
@@ -71,65 +50,7 @@ useEffect(() => {
         } )
          
     }, [setAlreadyVoted, user])
-     
-
     
-    /* function deleteVote(voteId) {
-        voteId.preventDefault()
-
-        console.log(voteId.target.voteId.value);
-
-        axios.delete(`https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes/${voteId.target.voteId.value}`)
-
-    } */
-
-    
-    function vote(e) {
-        e.preventDefault()
-        const vote = e.target.vote.value;
-        const message = e.target.message.value;
-        const voter = JSON.stringify(user.id);
-        
-        
-        const voteData = {}
-        voteData.vote = vote;
-        voteData.voter = voter;
-        voteData.message = message;
-
-        console.log(voteData);
-
-        axios.post("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes", voteData, null)
-
-        
-
-
-         /* axios.post("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes", voteData, null) */
- 
-          /* fetch("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes", {
-             method: "POST",
-             body: voteData
-         }).then(response => console.log(response)) */
-
-        navigate("/thanks") 
-    }
-    
-    /* console.log(user); */
-
-    function deleteEverything(e){
-        e.preventDefault();
-
-        axios.get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
-        .then(response => response.data)
-        .then(data => {
-            data?.map((votes) =>{
-                
-                axios.delete(`https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes/${votes._id}`)
-
-                return null;
-
-            })
-        })
-    }
 
     return (
         <>
