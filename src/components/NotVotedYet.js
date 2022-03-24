055c30be17cb3244afe8e2e7a4dd8ba771fd3372
Christian <chrnielsen2003@gmail.com>
2022-03-24 13:37:57 +0100

    .

--- src/components/NotVotedYet.js
+++ src/components/NotVotedYet.js
@@ -1,77 +1,66 @@
-import { navigate } from '@reach/router';
-import axios from 'axios';
-import { UserContext } from '../UserContext';
-import React, { useContext, useEffect, useState } from 'react';
-
+import { navigate } from "@reach/router";
+import axios from "axios";
+import { UserContext } from "../UserContext";
+import React, { useContext, useEffect, useState } from "react";
 
 export default function NotVotedYet() {
-    const { user } = useContext(UserContext)
-    const [medarbejdere, setMedarbejdere] = useState([])
-    const [voterble, setVoterble] = useState([]);
-    const [alfa, setAlfa] = useState(false)
-  
+  const { user } = useContext(UserContext);
+  const [medarbejdere, setMedarbejdere] = useState([]);
+  const [voterble, setVoterble] = useState([]);
+  const [alfa, setAlfa] = useState(false);
 
-    if(user === null) {
-        navigate("/")
-        // eslint-disable-next-line
-        location.reload();
-    }
-useEffect(() => {
-
-    if(user.id === 286828){
-        setAlfa(true)
+  if (user === null) {
+    navigate("/");
+    // eslint-disable-next-line
+    location.reload();
+  }
+  useEffect(() => {
+    if (user.id === 286828) {
+      setAlfa(true);
     } else {
-        setAlfa(false)
+      setAlfa(false);
     }
-}, [user])
+  }, [user]);
 
+  const fullName = user?.name;
+  const splitName = fullName.split(",");
+  const firstPartOfName = splitName[1];
+  const firstName = firstPartOfName.split(" ")[1];
 
-    const fullName = user?.name;
-    const splitName = fullName.split(',')
-    const firstPartOfName = splitName[1];
-    const firstName = firstPartOfName.split(" ")[1]
+  useEffect(() => {
+    axios
+      .get("/medarbejdere.json")
+      .then((response) => setMedarbejdere(response.data));
+  }, []);
 
-    
-    useEffect(()=> {
-        
-        axios.get('/medarbejdere.json')
-        .then(response => setMedarbejdere(response.data))
-    }, [])
-    
-    useEffect(()=> {
-        
-        setVoterble(medarbejdere.filter((e)=> e.leader === false))
-        
-    }, [medarbejdere, setMedarbejdere]); 
+  useEffect(() => {
+    setVoterble(medarbejdere.filter((e) => e.leader === false));
+  }, [medarbejdere, setMedarbejdere]);
 
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-
-    function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-        
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        
-        return array;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
-    shuffle(voterble)
-    
-    /* console.log(medarbejder); */
-    
-    
-    
-    /* useEffect(() => {
+
+    return array;
+  }
+  shuffle(voterble);
+
+  /* console.log(medarbejder); */
+
+  /* useEffect(() => {
 
         axios.get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
         .then(response => response.data)
@@ -91,10 +80,8 @@ useEffect(() => {
         } )
          
     }, [setAlreadyVoted, user]) */
-     
 
-    
-    /* function deleteVote(voteId) {
+  /* function deleteVote(voteId) {
         voteId.preventDefault()
 
         console.log(voteId.target.voteId.value);
@@ -103,106 +90,110 @@ useEffect(() => {
 
     } */
 
-    
-    function vote(e) {
-        e.preventDefault()
-        const vote = e.target.vote.value;
-        const message = e.target.message.value;
-        const voter = JSON.stringify(user.id);
-        
-        
-        const voteData = {}
-        voteData.vote = vote;
-        voteData.voter = voter;
-        voteData.message = message;
+  function vote(e) {
+    e.preventDefault();
+    const vote = e.target.vote.value;
+    const message = e.target.message.value;
+    const voter = JSON.stringify(user.id);
 
+    const voteData = {};
+    voteData.vote = vote;
+    voteData.voter = voter;
+    voteData.message = message;
 
-        axios.post("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes", voteData, null)
-        
+    axios.post(
+      "https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes",
+      voteData,
+      null
+    );
 
+    navigate("/thanks");
+  }
 
-        navigate("/thanks") 
-    }
-    
-    /* console.log(user); */
+  /* console.log(user); */
 
-    function deleteEverything(e){
-        e.preventDefault();
+  function deleteEverything(e) {
+    e.preventDefault();
 
-        axios.get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
-        .then(response => response.data)
-        .then(data => {
-            data?.map((votes) =>{
-                
-                axios.delete(`https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes/${votes._id}`)
+    axios
+      .get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
+      .then((response) => response.data)
+      .then((data) => {
+        data?.map((votes) => {
+          axios.delete(
+            `https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes/${votes._id}`
+          );
 
-                return null;
+          return null;
+        });
+      });
+  }
 
-            })
-        })
-    }
-  
-    function mailMessage(e){
+  /* function mailMessage(e){
         e.preventDefault();
         navigate("/winner")
-    }
-  
-    return (
-        <>
-            
-        <h1>Hej! { firstName }</h1>
+    } */
 
-  <form onSubmit={(e)=> vote(e)}>
-    <input list="vote-datalist" id="vote" name="vote" placeholder="Søg efter medarbejder" />
-   
+  return (
+    <>
+      <h1>Hej! {firstName}</h1>
 
-    <datalist id="vote-datalist">
-    
-    
-        {voterble.map((coworkers)=>{
+      <form onSubmit={(e) => vote(e)}>
+        <input
+          list="vote-datalist"
+          id="vote"
+          name="vote"
+          placeholder="Søg efter medarbejder"
+        />
+
+        <datalist id="vote-datalist">
+          {voterble.map((coworkers) => {
             const fullName = coworkers?.name;
-            const splitName = fullName.split(',')
+            const splitName = fullName.split(",");
             const firstPartOfName = splitName[1];
             const lastName = splitName[0];
-            
-            return(
-                <option key={coworkers.id} id={coworkers.id} value={coworkers.afdeling + firstPartOfName + " " + lastName}></option>
-            )
-            
-        })} 
-        <option id="options" value=""></option> 
-    </datalist>
-    <br />
-    <br />
-    <label htmlFor="message">Hvorfor skal han/hun vinde månedens medarbejder</label>
-    <br />
-    <br />
-    <textarea name="message" id="message" cols="30" rows="10"></textarea>
-    <br />
-    <br />
 
-    <input type="submit" value="Stem!" />
-</form>
-  
-  {/* <h1>Delete</h1>
+            return (
+              <option
+                key={coworkers.id}
+                id={coworkers.id}
+                value={coworkers.afdeling + firstPartOfName + " " + lastName}
+              ></option>
+            );
+          })}
+          <option id="options" value=""></option>
+        </datalist>
+        <br />
+        <br />
+        <label htmlFor="message">
+          Hvorfor skal han/hun vinde månedens medarbejder
+        </label>
+        <br />
+        <br />
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <br />
+        <br />
+
+        <input type="submit" value="Stem!" />
+      </form>
+
+      {/* <h1>Delete</h1>
   <form onSubmit={(voteId) => deleteVote(voteId)}>
     <input name="voteId" type="text" id="voteId"/>
     <input type="submit" value="Slet" />
   </form> */}
 
-
-{alfa ? (
-    <>
-        <button onClick={(e) => deleteEverything(e)}>Fjern alle stemmer</button>
-        <br />
-        <br />
-        <br />
-        {/* <button onClick={(e) => mailMessage(e)}>Send mail med månedens medarbejder</button> */}
+      {alfa ? (
+        <>
+          <button onClick={(e) => deleteEverything(e)}>
+            Fjern alle stemmer
+          </button>
+          <br />
+          <br />
+          <br />
+          {/* <button onClick={(e) => mailMessage(e)}>Send mail med månedens medarbejder</button> */}
+        </>
+      ) : null}
     </>
-) : ( 
-    <h1></h1>
- ) }      
-
-</>
-  )
+  );
 }
--- src/pages/Startside.js
+++ src/pages/Startside.js
@@ -1,80 +1,67 @@
-import { navigate } from '@reach/router';
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../UserContext';
+import { navigate } from "@reach/router";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../UserContext";
 
 export default function Startside() {
-    const { setUser } = useContext(UserContext)
-    const [medarbejdere, setMedarbejdere] = useState({})
-    const [votedlist, setVotedlist] = useState([])
+  const { setUser } = useContext(UserContext);
+  const [medarbejdere, setMedarbejdere] = useState([]);
+  const [votedlist, setVotedlist] = useState([]);
 
+  console.log(votedlist);
 
+  useEffect(() => {
+    axios
+      .get("/medarbejdere.json")
+      .then((response) => response.data)
+      .then((response) => setMedarbejdere(response));
 
-    useEffect(() => {
+    setUser(null);
+  }, [setMedarbejdere, setUser]);
 
-        axios.get("/medarbejdere.json")
-        .then(response => response.data)
-        .then((response) => setMedarbejdere(response))
+  useEffect(() => {
+    axios
+      .get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
+      .then((response) => response.data)
+      .then((response) => {
+        const votedList = response.map((e) => e.voter);
 
-        setUser(null)
+        setVotedlist(votedList);
+      });
+  }, [setVotedlist]);
 
-    }, [setMedarbejdere])
-    
-    useEffect(() => {
+  function login() {
+    const inputValue = document.getElementById("id");
+    if (inputValue.value.length !== 6) {
+      alert("Du skal skrive dit lønnummer");
+      return;
+    }
 
-        axios.get("https://foetex-hvidorevej-votes.herokuapp.com/api/v1/votes")
-        .then(response => response.data)
-        .then((response) => {
-            const votedList = response.map(e => e.voter)
+    const userLogIn = document.getElementById("id").value;
 
+    medarbejdere
+      ?.filter((userid) => userid.id === parseInt(userLogIn))
+      .map((currentUser) => setUser(currentUser));
 
-            setVotedlist(votedList)
+    navigate("/votingsite");
+  }
 
-        } )
-         
-    }, [setVotedlist])
+  return (
+    <>
+      <div id="logo">
+        <img src="/img/fotex-logo.svg" alt="idk" />
+        <div id="logoWhite"></div>
+      </div>
 
-      
-
-
-    function login() {
-        const inputValue =document.getElementById("id");
-        if(inputValue.value.length !== 6 ) {
-            alert("Du skal skrive dit lønnummer")
-            return;
-        } 
-       
-
-       const userLogIn = document.getElementById('id').value;
-
-     
-        medarbejdere?.filter(userid => userid.id === parseInt(userLogIn)).map(currentUser => setUser(currentUser)) 
-        
-        
-        navigate("/votingsite")
-   }
- 
-  return(
-  <>
-
-        <div id="logo">
-            <img src="/img/fotex-logo.svg" alt="idk" />
-            <div id="logoWhite"></div>
-        </div>
-
-        <div>
-            <h1>Skriv dit lønnummer</h1>
-            <br /> 
-            <input type="number" id="id" name="id" />
-            <br /> 
-            <br /> 
-            <br /> 
-            <button onClick={() => login()}>Log ind</button>
-        </div>
-
-
-  </>
-
-  ) 
-
+      <div>
+        <h1>Skriv dit lønnummer</h1>
+        <br />
+        <input type="number" id="id" name="id" />
+        <br />
+        <br />
+        <br />
+        <button onClick={() => login()}>Log ind</button>
+      </div>
+    </>
+  );
 }
